b2349f3c73fefa413bafcece849087959223d914
whirlwindaster <whirlwindaster@gmail.com>
2024-02-06 16:46:40 -0500

    ws established

---
+++ src/routes/er/game/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    let ws: WebSocket;
+
+    onMount(() => {
+        window.alert("didmount");
+        ws = new WebSocket(`ws://${import.meta.env.VITE_API_URL}ws?name=${localStorage.getItem("username")}&uuid=${localStorage.getItem("uuid")}`);
+
+        ws.onerror = () => {window.alert("noo")};
+        ws.onopen = () => {window.alert("yayy")};
+        ws.onmessage = (m) => {console.log(JSON.stringify(m))};
+    })
+</script>
+
+<div></div>
