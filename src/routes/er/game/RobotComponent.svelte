7e93e01439766c7290bdb045ddf2e4f72dcba468
whirlwindaster <whirlwindaster@gmail.com>
2024-02-07 18:51:35 -0500

    more good progress

---
+++ src/routes/er/game/RobotComponent.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import type { RobotColor } from "./types";
+    import { demonstrator } from "./stores";
+
+    export let robot: RobotColor | null;
+
+    let isMyTurn: boolean
+    demonstrator.subscribe((playerName) => {
+        if (playerName === localStorage.getItem("username")) {
+            isMyTurn = true;
+        }
+    })
+</script>
+
+<div>
+    {#if isMyTurn}
+        <p>my turn</p>
+    {/if}
+</div>
---
+++ src/routes/er/game/TileComponent.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import GoalComponent from "./GoalComponent.svelte";
+    import RobotComponent from "./RobotComponent.svelte";
+    import type { Coordinate, Tile, RobotColor} from "./types";
+
+    export let tile: Tile;
+</script>
+
+<div>
+    <GoalComponent goal={tile.goal} />
+    <RobotComponent robot={tile.robot} />
+</div>
